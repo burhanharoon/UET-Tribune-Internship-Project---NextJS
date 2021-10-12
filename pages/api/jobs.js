f089c099a22ad326392029e747b1d02609a4aaca
Burhan <2019cs626@student.uet.edu.pk>
2021-10-12 12:15:51 +0500

    Fixed some images

---
+++ pages/api/jobs.js
@@ -0,0 +1,52 @@
+export const jobs = [
+    {
+        id: 1,
+        image: "/top-jobs-logo/G.png",
+        title: "Software Engineer (Android), Libraries",
+        software: "Segment",
+        Location: "London, UK",
+        timePosted: 11,
+        salary: "$35k - $45k",
+        jobTime: "Full Time"
+    },
+    {
+        id: 2,
+        image: "/top-jobs-logo/illustrator.jpg",
+        title: "Recruiting Coordinator",
+        software: "Segment",
+        Location: "London, UK",
+        timePosted: 11,
+        salary: "$35k - $45k",
+        jobTime: "Full Time"
+    },
+    {
+        id: 3,
+        image: "/top-jobs-logo/in.png",
+        title: "Product Manager, Studio",
+        software: "Invision",
+        Location: "London, UK",
+        timePosted: 11,
+        salary: "$35k - $45k",
+        jobTime: "Full Time"
+    },
+    {
+        id: 4,
+        image: "https://www.saashub.com/images/app/service_logos/125/jyd3pirbz4w1/large.png?1579340184",
+        title: "Web Developer",
+        software: "Invision",
+        Location: "London, UK",
+        timePosted: 11,
+        salary: "$35k - $45k",
+        jobTime: "Full Time"
+    },
+    {
+        id: 5,
+        image: "https://logowik.com/content/uploads/images/upwork-icon.jpg",
+        title: "Software Product Designer",
+        software: "Invision",
+        Location: "London, UK",
+        timePosted: 11,
+        salary: "$35k - $45k",
+        jobTime: "Full Time"
+    },
+]
